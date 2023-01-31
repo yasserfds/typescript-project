0b23ef76dfd6fc63c0e297dd3f581d53130b51ef
TheGiantNinja <yasserfedsi34@gmail.com>
2023-01-31 23:15:27 +0100

    Almost done

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,30 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { addListener } from 'process';
+import { title } from 'process';
+import { v4 as uuidV4 } from 'uuid';
 
-import confetti from 'canvas-confetti';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+
+const list = document.querySelector<HTMLUListElement>('.list')
+const form = document.getElementById('new-task-form') as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>('.new-task-title')
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  if (input?.value == "" || input?.value == null) {
+    return;
+  };
+
+  const task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  addListItem(newTask)
+});
+
+function addListItem(task) {
+  
+}
